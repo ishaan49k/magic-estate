65c1edb2acb40f76e39ef8c8919595c391fb4e91
Ishaan Kulkarni <ishaan.kulkarni049@gmail.com>
2023-11-05 03:05:59 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -12,9 +13,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000!');
-//     }
-// )
-//   console.log('Server is running on port 3000!');
-// });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+});
+  
+app.use('/api/user', userRouter);
