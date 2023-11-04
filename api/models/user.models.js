d18f742103cf618d11d9ab233138bce4c4749717
Ishaan Kulkarni <ishaan.kulkarni049@gmail.com>
2023-11-05 03:41:20 +0530

    added signin api route

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -1,26 +1,25 @@
 import mongoose from 'mongoose';
 
-// create a schema for a user
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    passoword: {
-        type: String,
-        required: true,
-    }
-    // timestamp will be used to sort the user based on time of creation and update later on
-}, { timestamps: true });
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-
-// crete a model using the schema
 const User = mongoose.model('User', userSchema);
 
 export default User;
