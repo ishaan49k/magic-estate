ea4aa3eb1a0c4b7ed57252520a2be6592e824bde
Ishaan Kulkarni <ishaan.kulkarni049@gmail.com>
2023-11-06 05:30:23 +0530

    updated header and made profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,25 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'; 
-import SignIn from './pages/SignIn'; 
-import SignUp from './pages/SignUp'; 
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-// import PrivateRoute from './components/PrivateRoute'; 
-// import CreateListing from './pages/CreateListing'; 
-// import UpdateListing from './pages/UpdateListing'; 
-// import Listing from './pages/Listing'; 
-// import Search from './pages/Search';
+import PrivateRoute from './components/PrivateRoute';
 
-
-
-
-
-
-const App = () => {
+export default function App() {
   return (
-<BrowserRouter>
-  <Header/>
-  <Routes>
-    {/* Cover everything with BrowserRouter and Routes and then include all the routes */}
-    <Route path='/' element={<Home />} /> 
-    <Route path='/sign-in' element={<SignIn />} /> 
-    <Route path='/sign-up' element={<SignUp />} />
-    <Route path='/about' element={<About />} /> 
-    <Route path='/profile' element={<Profile />} /> 
-    {/* <Route path='/search' element={<Search />} />  */}
-    {/* <Route path='/listing/:listingId' element={<Listing />} /> */}
-
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
-
-
-
-
-// export default function App(){
-//   return 
-
-// }
