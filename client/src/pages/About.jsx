dd64d6591edb1d7f95b0c3893b5d13d9f2646b28
Ishaan Kulkarni <ishaan.kulkarni049@gmail.com>
2023-11-09 02:02:47 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-const About = () => {
-    return (
-        <div>
-            <h1>about</h1>
-        </div>
-    );
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Ishaan</h1>
+      <p className='mb-4 text-slate-700'>Hi there! I am Ishaan Kulkarni, a final year Computer Science and Engineering student at NSUT, New Delhi.</p>
+      <p className='mb-4 text-slate-700'>
+      I hope you found this MERN project interesting. 
+      </p>
+      <p className='mb-4 text-slate-700'>For any inquiries, please contact me at ishaan . kulkarni . ug20 @ nsut.ac.in</p>
+    </div>
+  )
 }
-
-export default About;
